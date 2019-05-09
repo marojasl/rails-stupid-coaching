76f04a79253ef52acf2663cc5e2423344715d7bf
Maria Antonia Rojas <marojaslondono@gmail.com>
2019-05-09 13:50:59 +0100

    Input question and get a response Stupid Coach Exercise

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  # get 'question/ask', as: 'ask'
+  # get 'question/answer', as: 'answer'
+  # " as: 'ask' " es para que cuando cree un link, el path sea ask_path
+  get 'ask', to: 'questions#ask', as: 'ask'
+  get 'answer', to: 'questions#answer', as: 'answer'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
